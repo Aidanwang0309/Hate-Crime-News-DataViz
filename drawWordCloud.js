f5f2e39985f69be76de0c00bf9986daf3f34017e
Jingfei Lin <jade@Jingfeis-MacBook-Pro.local>
2017-10-05 09:21:49 -0400

    final

--- drawWordCloud.js
+++ drawWordCloud.js
@@ -1,5 +1,6 @@
 var inputValue = null;
 var month = ["Feb","Mar","Apr","May","Jun","Jul","Aug"];
+var monthName = ["Feburary","March","April","May","June","July","August"];
 
 // when the input range changes update the value
 d3.select("#timeslide").on("input", function() {
@@ -8,7 +9,7 @@ d3.select("#timeslide").on("input", function() {
 
 
 function update(value) {
-    document.getElementById("range").innerHTML=month[value];
+    document.getElementById("range").innerHTML=monthName[value];
     inputValue = month[value];
     console.log(inputValue);
     changeJson(inputValue);
@@ -18,19 +19,19 @@ function update(value) {
 
 
 var width = 1050,
-    height = 800;
+    height = 700;
 
 
 // var width = winWidth*0.7,
 //     height = width*0.6;
 
-var fill = d3.scale.category20();
+var fill = d3.scale.category10();
 
 
 d3.json('FebKW.json',function(data){
   var words=[]
   data.forEach(function(element){
-    words.push({text:element.word,size:element.count})
+    words.push({text:element.word,size:element.count,titles:element.titles,urls:element.urls})
   });
   // console.log(words);
 
@@ -54,8 +55,6 @@ d3.json('FebKW.json',function(data){
 
 
 
-
-
 function drawCloud(words) {
     d3.select("#word-cloud").select("svg")
     .attr("width", width)
@@ -74,6 +73,59 @@ function drawCloud(words) {
     })
     .text(function(d) { return d.text; });
 
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        //not d.size,  should be .count
+        return "<strong>"+d.text+"</strong><br>"+"<strong> news count:</strong> <span style='color:red'>" + d.titles.length + "</span>";
+      })
+    // console.log(tip);
+    var svg=d3.select("body").select("svg");
+    svg.call(tip);
+    //mouseover
+    // svg.selectAll("text").on('mouseover',tip.show).on('mouseout', tip.hide);
+    svg.selectAll("text").on({
+          "mouseover": tip.show,
+          "mouseout":  tip.hide,
+          "click":  function(d) {
+            $( "a" ).remove();
+            $("br").remove();
+            $("h4").remove();
+            // var p=$( "<br><p>News reports with keyword "+d.text+" are listed below:"+"</p><br>" ).insertAfter( "#range" );
+            // p.attr("id","intro");
+
+             for(var i=0;i<=d.titles.length;i++){
+               var infoP;
+               if(i==0){
+                 var h4=$( "<h4>News reports with keyword "+d.text+" are listed below:"+"</h4>" ).insertAfter( "#range" );
+                 h4.attr("id","info");
+                 infoP=$( "<a></a>" );
+                  h4.after(infoP);
+               }
+               else{
+                 infoP=$( "<a></a><br>" );
+                 h4.after(infoP);
+               }
+
+               infoP.html(d.titles[i]);
+               infoP.attr("href", d.urls[i]);
+               infoP.attr("id", "info");
+             }
+            //  var infoP=$( "<p></p>" ).insertAfter( "#range" );
+            //
+            // //  infoDiv.attr('id','word-info');
+            // //  infoDiv.offset({top: 200,left: 20});
+            // //  infoDiv.attr('style',  'background-color:gray');
+            // //  infoDiv.width(100);
+            // //  infoDiv.height(400);
+            // // //  div.style.backgroundColor = "rgba(0, 255, 0, 0.3)";
+            // // //  div.style.position = "absolute";
+            // // //  div.style.left = "50px";
+            // // //  div.style.top = "50px";
+            // infoP.html(d.titles[0]);
+           },
+        });
 
 }
 
@@ -83,7 +135,7 @@ function changeJson(month){
   d3.json(month+'KW'+'.json',function(data){
     var words=[]
     data.forEach(function(element){
-      words.push({text:element.word,size:element.count})
+      words.push({text:element.word,size:element.count,titles:element.titles,urls:element.urls})
     });
 
   console.log(words);
