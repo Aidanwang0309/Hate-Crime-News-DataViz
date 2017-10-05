5be3e6052f622b3cd2cda1c83ab7b01afa8b1095
Jingfei Lin <jade@Jingfeis-MacBook-Pro.local>
2017-10-05 05:53:14 -0400

    change with month

--- csvToJson.py
+++ csvToJson.py
@@ -1,4 +1,3 @@
-# Code courtesy: https://stackoverflow.com/questions/19697846/python-csv-to-json
 import csv, os
 import json
 import sys
@@ -9,50 +8,83 @@ path=r"hate crime index data/"
 
 keywordsList=[];
 jsonKW=[];
-with open(path+'May.csv') as fin:
+
+
+
+with open(path+'Aug.csv') as fin:
     csvin = csv.reader(fin)
     header = next(csvin, [])
-    json.dump(dict(zip(header, zip(*csvin))),open("May.json","w"),indent=4)
+    json.dump(dict(zip(header, zip(*csvin))),open("Aug.json","w"),indent=4)
 
-jsonfile=open("May.json","r")
+jsonfile=open("Aug.json","r")
 
 
 with jsonfile as data_file:
     data = json.load(data_file)
+
     for i in range(len(data["Keywords"])):
-        keywords=data["Keywords"][i].split()
-        #if Keywords in csv have the same ones, reduce redundancy
-        wordsPerLine=[]
-        for word in keywords:
-            if word not in wordsPerLine:
-                wordsPerLine.append(word)
-                if word not in keywordsList:
-                    keywordsList.append(word)
-                    newWord={}
-                    newWord["word"]=word
-                    newWord["titles"]=[]
-                    newWord["titles"].append(data["Article Title"][i])
-                    newWord["urls"]=[]
-                    newWord["urls"].append(data["URL"][i])
-                    newWord["dates"]=[]
-                    newWord["dates"].append(data["Article Date"][i])
-                    jsonKW.append(newWord)
+
+        if(data["Keywords"][i]==""):
+            # keywords=re.split('; |, |\*|\n',data["Article Title"][i].lower())
+            keywords=data["Article Title"][i].lower().split()
+
+            wordsPerLine=[]
+            #if Keywords in csv have the same ones, reduce redundancy
+            for word in keywords:
+                if word not in wordsPerLine:
+                    wordsPerLine.append(word)
+                    if word not in keywordsList:
+                        keywordsList.append(word)
+                        newWord={}
+                        newWord["word"]=word
+                        newWord["titles"]=[]
+                        newWord["titles"].append(data["Article Title"][i])
+                        newWord["urls"]=[]
+                        newWord["urls"].append(data["URL"][i])
+                        newWord["dates"]=[]
+                        newWord["dates"].append(data["Article Date"][i])
+                        jsonKW.append(newWord)
+                    else:
+                        wordIndex=keywordsList.index(word)
+                        jsonKW[wordIndex]["titles"].append(data["Article Title"][i])
+                        jsonKW[wordIndex]["urls"].append(data["URL"][i])
+                        jsonKW[wordIndex]["dates"].append(data["Article Date"][i])
                 else:
-                    wordIndex=keywordsList.index(word)
-                    jsonKW[wordIndex]["titles"].append(data["Article Title"][i])
-                    jsonKW[wordIndex]["urls"].append(data["URL"][i])
-                    jsonKW[wordIndex]["dates"].append(data["Article Date"][i])
-            else:
-                continue
+                    continue
+        else:
+            keywords=data["Keywords"][i].split()
+            #if Keywords in csv have the same ones, reduce redundancy
+            wordsPerLine=[]
+            for word in keywords:
+                if word not in wordsPerLine:
+                    wordsPerLine.append(word)
+                    if word not in keywordsList:
+                        keywordsList.append(word)
+                        newWord={}
+                        newWord["word"]=word
+                        newWord["titles"]=[]
+                        newWord["titles"].append(data["Article Title"][i])
+                        newWord["urls"]=[]
+                        newWord["urls"].append(data["URL"][i])
+                        newWord["dates"]=[]
+                        newWord["dates"].append(data["Article Date"][i])
+                        jsonKW.append(newWord)
+                    else:
+                        wordIndex=keywordsList.index(word)
+                        jsonKW[wordIndex]["titles"].append(data["Article Title"][i])
+                        jsonKW[wordIndex]["urls"].append(data["URL"][i])
+                        jsonKW[wordIndex]["dates"].append(data["Article Date"][i])
+                else:
+                    continue
 
 #remove the word from the cloud if frequency is too low
 newJson=[]
-newJson = [x for x in jsonKW if len(x["dates"])>=5 ]
+newJson = [x for x in jsonKW if len(x["dates"])>=4 ]
 for element in newJson:
     element["count"]=len(element["dates"])
 
 print len(newJson)
 
 
-with open('MayKW.json', 'w') as fout:
+with open('AugKW.json', 'w') as fout:
     json.dump(newJson, fout,indent=4)
--- drawWordCloud.js
+++ drawWordCloud.js
@@ -1,3 +1,22 @@
+var inputValue = null;
+var month = ["Feb","Mar","Apr","May","Jun","Jul","Aug"];
+
+// when the input range changes update the value
+d3.select("#timeslide").on("input", function() {
+    update(+this.value);
+});
+
+
+function update(value) {
+    document.getElementById("range").innerHTML=month[value];
+    inputValue = month[value];
+    console.log(inputValue);
+    changeJson(inputValue);
+    // d3.selectAll(".incident")
+    //     .attr("fill", dateMatch);
+}
+
+
 var width = 1050,
     height = 800;
 
@@ -8,12 +27,12 @@ var width = 1050,
 var fill = d3.scale.category20();
 
 
-d3.json('JulKW.json',function(data){
+d3.json('FebKW.json',function(data){
   var words=[]
   data.forEach(function(element){
     words.push({text:element.word,size:element.count})
   });
-  console.log(words);
+  // console.log(words);
 
   // font scale
   //array of count of each element in json file
@@ -33,58 +52,8 @@ d3.json('JulKW.json',function(data){
       .start();
 });
 
-//button to swap over datasets
-// d3.select("#word-cloud").append("button")
-//     .text("change data")
-//     .on("click",function(){
-//         //select new data
-//         d3.json('MayKW.json',function(data){
-//           var words=[]
-//           data.forEach(function(element){
-//             words.push({text:element.word,size:element.count})
-//           });
-//           console.log(words);
-//
-//           // font scale
-//           //array of count of each element in json file
-//           var arrCount = data.map(function(d){return d.count});
-//           console.log(arrCount);
-//           var fontScale = d3.scale.linear()
-//               .domain([d3.min(arrCount),d3.max(arrCount)])
-//               .range([30,90])
-//
-//         //rejoin data
-//         var text = svgDoc.select("g").selectAll("text")
-//             .data(data);
-//
-//         text.exit().remove();//remove unneeded texts
-//         // text here is the elements already on the page, we just update them
-//         text.attr("class", "update");
-//         text.enter().append("text")
-//             .style("font-size", function(d) { return d.size + "px"; })
-//             .style("font-family", "Impact")
-//             .style("fill", function(d, i) { return fill(i); })
-//             .attr("text-anchor", "middle")
-//             .attr("transform", function(d) {
-//                 return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-//             })
-//             .text(function(d) { return d.text; });//create any new texts needed
-//
-//         // //update all texts to new positions
-//         // text.transition()
-//         //     .duration(500)
-//         //     .attr("cx",function(d,i){
-//         //         var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-//         //         return xBuffer+(i*spacing)
-//         //     })
-//         //     .attr("cy",yBuffer)
-//         //     .attr("r",function(d,i){return d});
-//         //
-//         // d3.select("text").text("dataset"+dataIndex);
-//
-//     });
-//   );//end onclick function
-//
+
+
 
 
 function drawCloud(words) {
@@ -109,29 +78,34 @@ function drawCloud(words) {
 }
 
 
-function changeJson(){
-  d3.json('MayKW.json',function(data){
+function changeJson(month){
+  //select new data
+  d3.json(month+'KW'+'.json',function(data){
     var words=[]
     data.forEach(function(element){
       words.push({text:element.word,size:element.count})
     });
-    console.log(words);
 
-    // font scale
-    //array of count of each element in json file
-    var arrCount = data.map(function(d){return d.count});
-    console.log(arrCount);
-    var fontScale = d3.scale.linear()
-        .domain([d3.min(arrCount),d3.max(arrCount)])
-        .range([30,90])
+  console.log(words);
 
-    d3.layout.cloud().size([width, height])
-        .words(words)
-        .padding(0)
-  //      .rotate(function() { return ~~(Math.random() * 2) * 90; })
-        .font("Impact")
-        .fontSize(function(d) { return fontScale(d.size); })
-        .on("end", drawCloud)
-        .start();
-  });
+  // font scale
+  //array of count of each element in json file
+  var arrCount = data.map(function(d){return d.count});
+  console.log(arrCount);
+  var fontScale = d3.scale.linear()
+      .domain([d3.min(arrCount),d3.max(arrCount)])
+      .range([30,90])
+
+  d3.select("svg").selectAll("g").remove();
+
+  d3.layout.cloud().size([width, height])
+      .words(words)
+      .padding(0)
+
+      .font("Impact")
+      .fontSize(function(d) { return fontScale(d.size); })
+      .on("end", drawCloud)
+      .start();
+
+});
 }
